310c79b33d9e5297b75c5bff8194ad339afbe591
ocpro <heze325@qq.com>
2019-10-20 11:07:10 +0800

    Add:基于SVM的手写数字识别案例代码实现

---
+++ sklearn11/1201/01_案例代码：基于SVM的手写数字识别案例代码实现.py
@@ -0,0 +1,50 @@
+# -- encoding:utf-8 --
+"""
+@File : 01_案例代码：基于SVM的手写数字识别案例代码实现
+@Author: Octal_H
+@Date : 2019/10/20
+@Desc : 
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.svm import SVC
+from sklearn.externals import joblib
+
+# 1.加载数据
+digits = datasets.load_digits()
+# 总样本数目：1797张0~9的数字图像，每个图像的大小: 8*8, 也就是使用64个像素点来表示图像信息
+print(digits.data.shape)
+print(digits.images.shape)
+print(digits.target.shape)
+# print(digits)
+
+# # 随便看几个图片
+# idx = 100
+# print(digits.images[idx])
+# # plt.imshow(digits.images[idx], cmap=plt.cm.gray)
+# # 颜色反转
+# plt.imshow(digits.images[idx], cmap=plt.cm.gray_r)
+# plt.title(digits.target[idx])
+# plt.show()
+
+# 2.数据的划分
+n_samples = digits.images.shape[0]
+X = digits.data
+Y = digits.target
+split_index = 1 * n_samples // 2
+x_train, x_test = X[:split_index], X[split_index:]
+y_train, y_test = Y[:split_index], Y[split_index:]
+print("训练数据格式:{}".format(np.shape(x_train)))
+print("测试数据格式:{}".format(np.shape(x_test)))
+
+# 3.模型构建
+algo = SVC(kernel='rbf', C=1.0, gamma=0.001)
+algo.fit(x_train, y_train)
+
+# 3.模型效果查看
+print("训练数据上的效果:{}".format(algo.score(x_train, y_train)))
+print("测试数据上的效果:{}".format(algo.score(x_test, y_test)))
+
+# 模型持久化
+joblib.dump(algo, './models/svm_digits.pkl')
